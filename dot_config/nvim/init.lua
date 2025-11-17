176b7226e4891c7806060f9874f120469578dd3f
chrisnathan-rokt <chris.nathan@rokt.com>
2025-11-17 17:24:24 -0500

    Update dotfiles: lsd config, nvim init, zsh configs, and add zprofile and zshrc_prompt

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,49 +1,10 @@
--- Set leader key to space
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Neovim configuration entry point
 
--- Set options
-vim.o.number = true
-vim.o.relativenumber = false
-vim.o.ignorecase = true
-vim.o.smartcase = true
+-- Load options
+require("options")
 
--- Add lazy.nvim to runtime path
-vim.opt.rtp:prepend("~/.config/nvim/lazy/lazy.nvim")
+-- Load plugins (this will install lazy.nvim if needed)
+require("plugins.init")
 
--- Setup plugins
-require("lazy").setup({
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-    -- File tree explorer
-    {
-        "nvim-tree/nvim-tree.lua",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("nvim-tree").setup({
-                view = {
-                    side = "left",
-                    width = 30,
-                },
-            })
-        end,
-    },
-
-    -- Telescope fuzzy finder
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
-})
-
--- Set colorscheme
-vim.g.catppuccin_flavour = "mocha" -- optional: latte, frappe, macchiato, mocha
-vim.cmd[[colorscheme catppuccin]]
-
--- Keybindings
--- Leader + e to toggle file tree explorer
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Toggle file tree" })
-
--- Leader + ff to open Telescope find files
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true, desc = "Telescope find files" })
+-- Load keymaps
+require("keymaps")
