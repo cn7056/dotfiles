920a9caf76e41f22bb13fa56bc169a73d494545e
Chris Nathan <chrisnathan@unifi.localdomain>
2025-11-16 19:01:49 -0500

    Update nvim config: init.lua and lazy-lock.json

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,45 +1,49 @@
--- init.lua
-vim.wo.number = true
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.loop or vim.uv).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--branch=stable",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Leader keys
+-- Set leader key to space
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = " "
 
--- lazy.nvim setup
+-- Set options
+vim.o.number = true
+vim.o.relativenumber = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Add lazy.nvim to runtime path
+vim.opt.rtp:prepend("~/.config/nvim/lazy/lazy.nvim")
+
+-- Setup plugins
 require("lazy").setup({
-  spec = {
-    { "folke/tokyonight.nvim" }, -- theme plugin
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+    -- File tree explorer
     {
-     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("nvim-tree").setup({
+                view = {
+                    side = "left",
+                    width = 30,
+                },
+            })
+        end,
     },
+
+    -- Telescope fuzzy finder
     {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-    }
-  },
-  install = { colorscheme = { "tokyonight" } },
-  checker = { enabled = true },
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
 })
 
 -- Set colorscheme
-vim.cmd([[colorscheme tokyonight]])
-local telescope = require('telescope.builtin')
+vim.g.catppuccin_flavour = "mocha" -- optional: latte, frappe, macchiato, mocha
+vim.cmd[[colorscheme catppuccin]]
 
-vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Find files' })
-vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR> path=%:p:h select_buffer=true<CR>")
+-- Keybindings
+-- Leader + e to toggle file tree explorer
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Toggle file tree" })
 
+-- Leader + ff to open Telescope find files
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true, desc = "Telescope find files" })
